ac2c101ca82b1c6e8300f16eb2b44eeaff3aa4d2
cedrichansen <ceddy29@yahoo.ca>
2018-12-10 23:10:56 -0500

    making the Space class serializable as well

--- src/main/java/Space.java
+++ src/main/java/Space.java
@@ -1,6 +1,7 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Space {
+public class Space implements Serializable {
 
     private         int                     colour;
     private         boolean                 isEncapsulated;
